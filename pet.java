0affae5c17fbe8bc1a03c8f8ff3a79baebfcce5e
Lesly-Gonzalez <113069585+Lesly-Gonzalez@users.noreply.github.com>
2022-11-16 20:12:09 -0500

    Completed assignment

--- pet.java
+++ pet.java
@@ -1,5 +1,8 @@
 public class pet extends PetShelter {
 
+    /** Class constructor 
+     * @param pet's name, age, type of animal, breed, health and time it has been in the shelter.
+     */
     public pet(String name, int age, String animalType, String breed, String health, int timeInShelter) {
         super(name, age, animalType, breed, health, timeInShelter);
 
@@ -9,5 +12,5 @@ public class pet extends PetShelter {
         pet Tangbao = new pet("Tangbao", 1, "Cat", "Domestic Short Hair", "Good", 1);
         System.out.println(Tangbao);
     }
-
+ 
 }
